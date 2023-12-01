fed5371915c2cec80c171e18772d7563fc4d13cd
Jarod DeWeese <jaroddeweese@gmail.com>
2023-12-01 04:11:50 +0000

    change'

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/jdeweese1/haven/main/setup.sh)"
 sudo yum install -y  python3.11 git make jq zsh
 python3 -m ensurepip
+python3 -m pip install --upgrade pip
 python3 -m pip install --user ansible
 git clone https://github.com/jdeweese1/dotfiles
 (cd dotfiles && make dotfiles)
@@ -13,7 +14,7 @@ git clone https://github.com/jdeweese1/exam-template
 
 sudo tailscale up
 key_base_name=~/.ssh/$(date +"%Y-%m-%d")_ed25519
-ssh-keygen -t ed25519 -C "your_email@example.com" -f $key_base_name
+ssh-keygen -t ed25519 -C "your_email@example.com" -f $key_base_name -q -N ""
 eval "$(ssh-agent -s)"
 ssh-add key_base_name
 echo "You should add the following to ur github:"
